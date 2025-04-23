3d994cefc39a8b828b41a7b0352e860d643720c6
Saurabh Rathod <saurabh.r@prometteur.in>
2025-04-23 11:47:21 +0530

    changing bundle.js line 12159 https to http

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12156,7 +12156,7 @@ var checkedAssets, assetsToAccept;
 var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
-  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var protocol = location.protocol === 'http:' ? 'wss' : 'ws';
   var ws = new WebSocket(protocol + '://' + hostname + ':' + "62314" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
